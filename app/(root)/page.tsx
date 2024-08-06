4b47274df06d3b61fd5466c17753050446242a76
Mustafa Furkan BİLEN <furkanbilen4@gmail.com>
2024-08-06 16:44:03 +0300

    Implement sidebars and layouts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,9 +2,10 @@ import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 import { type ClassValue } from 'clsx';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
+import RightSidear from '@/components/RightSidear';
 
 const Home = () => {
-  const loggedIn = {firstName:"Mustafa"};
+  const loggedIn = {firstName:"Mustafa", lastName:"Bilen",email:"mustafa@java.com"};
   return (
     <section className='home'>
       <div className='home-content'>
@@ -21,7 +22,13 @@ const Home = () => {
             totalCurrentBalance={1250.45}
           />
         </header>
+        RECENT TRANSACTIONS 
       </div>
+      <RightSidear 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance:1234.544},{currentBalance:4343.456}]}
+      />
     </section>
   )
 }
